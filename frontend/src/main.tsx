56824211e71bacd38c34c84093bfe36354005730
NorihiroTanaka <norohiro1998@gmail.com>
2025-09-04 21:31:52 +0900

    jsxからtsxに変更

---
+++ frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+let root = document.getElementById('root');
+if (!root) {
+  root = document.createElement('div');
+  root.id = 'root';
+  document.body.appendChild(root);
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
