c684fca911617ba8f867d628bc947402b7737318
NorihiroTanaka <norohiro1998@gmail.com>
2025-09-04 21:25:03 +0900

    サイドメニューアイテムリストにフッター追加

--- frontend/src/components/SideMenu.tsx
+++ frontend/src/components/SideMenu.tsx
@@ -4,15 +4,21 @@ import { VscSettingsGear } from 'react-icons/vsc';
 import { TfiMenu, TfiClose } from 'react-icons/tfi';
 import {Sidebar, Menu, MenuItem, SubMenu} from 'react-pro-sidebar';
 
-const MenuItemList = [
-    { icon: <FaSearch />, label: "検索・一覧"},
-    { icon: <FaPencilAlt />, label: "追加・編集"},
-    { icon: <VscSettingsGear/>, label: "設定"}
-];
+const MenuItemList = {
+    header: [
+    ],
+    body: [
+        { icon: <FaSearch />, label: "検索・一覧"},
+        { icon: <FaPencilAlt />, label: "追加・編集"},
+    ],
+    footer: [
+        { icon: <VscSettingsGear/>, label: "設定"}
+    ]
+}
 
-const BuildMenuItems = () => {
+const BuildMenuItems = (layout: string) => {
     return (
-        MenuItemList.map((item, index) => (
+        MenuItemList[layout].map((item, index) => (
             <MenuItem icon={item.icon} key={index}>
                 {item.label}
             </MenuItem>
@@ -29,17 +35,18 @@ const SideMenu: React.FC = () => {
 
     return (
         <>
-            <Sidebar collapsed={sidebarCollapsed} >
-                <Menu className='sidebar_header' style={{ borderBottom: '1px solid #ccc' }}>
+            <Sidebar collapsed={sidebarCollapsed}>
+                <Menu className="sidebar_header" style={{ borderBottom: '1px solid #ccc', marginTop: 5}}>
                     <MenuItem onClick={toggleSidebar} icon={sidebarCollapsed ? <TfiMenu/> : <TfiClose/>}>mAst</MenuItem>
+                    {BuildMenuItems("header")}
                 </Menu>
                 <Menu className="sidebar_body">
-                    {BuildMenuItems()}
+                    {BuildMenuItems("body")}
                 </Menu>
             </Sidebar>
-            <Sidebar collapsed={sidebarCollapsed} style={{ borderTop: '1px solid #ccc', marginTop: 'auto' }} >
+            <Sidebar collapsed={sidebarCollapsed} style={{ borderTop: '1px solid #ccc', bottom: '0%', marginTop: 'auto', marginBottom: 10 }}>
                 <Menu className="sidebar_footer">
-                    <MenuItem icon={<VscSettingsGear/>}>設定</MenuItem>
+                    {BuildMenuItems("footer")}
                 </Menu>
             </Sidebar>
         </>
